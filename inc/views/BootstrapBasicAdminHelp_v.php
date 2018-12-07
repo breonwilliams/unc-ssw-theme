a5dfb9965a502582d67779e9af9f59472c2482af
breonw <breon_williams@unc.edu>
2018-12-07 08:26:34 -0500

    modify help page

--- inc/views/BootstrapBasicAdminHelp_v.php
+++ inc/views/BootstrapBasicAdminHelp_v.php
@@ -6,14 +6,95 @@
 	
 	<h3>Bootstrap features</h3>
 	<p>This theme can use all Bootstrap classes, elements and styles.</p>
-	
-	<h3>Responsive image</h3>
-	<p>For responsive image please add img-responsive class to img element. Example: <code>&lt;img src=&quot;...&quot; alt=&quot;&quot; class=&quot;img-responsive&quot;&gt;</code></p>
 
-	<h3>Responsive video</h3>
-	Cloak video element (video element or embeded video) with <code>&lt;div class=&quot;flexvideo&quot;&gt;...your video...&lt;/div&gt;</code>.
+	<h3>Shortcodes</h3>
+	<p>All shortcodes can be found in the WYSIWYG "Shortcodes" dropdown select.</p>
+
+	<p><strong>[background_vid poster="" mp4="" padding="pad-100" overlay=""][/background_vid]</strong></p>
+	<p>Adds a full width section using a video as background</p>
+
+	<p><strong>[color_section bgcolor="" class=""][/color_section]</strong></p>
+	<p>Adds a full width section using a background color.</p>
+
+	<p><strong>[img_section bgimg="" class="" overlay=""][/img_section]</strong></p>
+	<p>Adds a full width section using a background image.</p>
+
+	<p><strong>[parallax_section bgimg="" class="" overlay=""][/parallax_section]</strong></p>
+	<p>Adds a full width section using a parallax background image.</p>
+
+	<p><strong>[square_section class="" style="" bg-img=""][/square_section]</strong></p>
+	<p>Adds a fixed square background image with overlaying text.</p>
+
+	<p><strong>[boot_modal button="Modal button" title="Modal Title" class="btn btn-primary" target="modal1" closeclass="btn btn-default" modalsize="modal-lg"][/boot_modal]</strong></p>
+	<p>Adds a bootstrap modal.</p>
+
+	<p><strong>[search_overlay]</strong></p>
+	<p>Adds a popup search overlay</p>
+
+	<p><strong>[popup_video class="" url=""][/popup_video]</strong></p>
+	<p>Adds a popup video link.</p>
+
+	<p><strong>[popup_playbutton class="" url=""][/popup_playbutton]</strong></p>
+	<p>Adds a Carolina blue popup video play button.</p>
+
+	<p><strong>[events-tribe-list cat="" number="2" class=""]</strong></p>
+	<p>Adds a list of recent events.</p>
+
+	<p><strong>[unc_taxonomy_list taxonomy="staff_category" post-type="staff" title=""]</strong></p>
+	<p>Adds an alphabetical list of taxonomies</p>
+
+	<p><strong>[ct_terms custom_taxonomy="staff_category"]</strong></p>
+	<p>Adds an hierarchical list of taxonomies</p>
+
+	<p><strong>[list_recent_posts category="" class="" ptype="" per_page="4"]</strong></p>
+	<p>Simple recent posts list.</p>
+
+	<p><strong>[list_recent_posts_horiz category="" class="" ptype="" per_page="4"]</strong></p>
+	<p>Adds styled horizontal list of recent posts</p>
+
+	<p><strong>[thumb_recent_posts column="col-md-4" class="" ptype="" per_page="4"]</strong></p>
+	<p>Adds recent post using bootstrap thumbnails with fixed height.</p>
+
+	<p><strong>[thumb_recent_posts_fluid column="col-md-4" class="" ptype="" per_page="4"]</strong></p>
+	<p>Adds recent post using bootstrap thumbnails with fluid height.</p>
+
+	<p><strong>[carousel_recent_posts class="slick-1" category="" ptype="" per_page="8"]</strong></p>
+	<p>Adds recent post carousel.</p>
+
+	<p><strong>[relatedposts ptype="" max="4" column="col-md-4" class="" orderby="rand" label="Related Posts"]</strong></p>
+	<p>Adds related post with masonry.</p>
+
+	<p><strong>[datatables_recent_staff staff_category="" posts="-1" ptype="staff"]</strong></p>
+	<p>Adds a datatables list of staff members.</p>
+
+	<p><strong>[googlemap id="myMap1" height="" zoom="15" lat="35.905160" long="-79.046908" title="The University of North Carolina at Chapel Hill"]</strong></p>
+	<p>Adds a google to your posts/page. Can be added to the full width footer section in pages.</p>
+
+	<p><strong>[custom_div class="" style=""][/custom_div]</strong></p>
+	<p>Adds a custom div. Custom class can be added to style and format.</p>
+
+	<p><strong>[listmenu menu="" menu_class=""]</strong></p>
+	<p>Creates a custom menu. Simply add a new menu to you site and copy and paste the name of that menu in the menu="" attribute. Classes can be added to format.</p>
+
+	<p><strong>[carousel_wrap class="slick-1"][/carousel_wrap]</strong></p>
+	<p>Container for the carousel items.</p>
+
+	<p><strong>[carousel_item class=""][/carousel_item]</strong></p>
+	<p>Container for each item in the carousel.</p>
+
+	<p><strong>[boot_logged_in]This is what user sees when logged in[/boot_logged_in]</strong></p>
+	<p>Shortcode that can be used with membership sites. This is what user sees when logged in.</p>
+
+	<p><strong>[boot_logged_out]This is what user sees when logged out[/boot_logged_out]</strong></p>
+	<p>Shortcode that can be used with membership sites. This is what user sees when logged out.</p>
+
+	<p><strong>[boot_login_form label_log_in="Login"]</strong></p>
+	<p>Form used to login.</p>
+
+	<p><strong>[boot_logoutbtn linktext="Log Out" class="btn btn-primary"]</strong></p>
+	<p>Logout button.</p>
+	
 
 	<?php do_action('bootstrapbasic_theme_help_content'); ?>
 	
-	<p style="margin-top: 20px;"><span style="font-size: 1.2rem;">&#128147;</span> If my theme can safe your life, your jobs, your projects please <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=9HQE4GVV4KTZE" target="donations_link">buy me some foods</a>.</p>
 </div>
